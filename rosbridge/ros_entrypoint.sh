2779203ed0d5302db32c7f5c6feb0263b1f1ec4a
Sebastián Zapata <seba-za@hotmail.com>
2019-12-25 02:06:37 -0300

    Cambiados los ROS_HOSTNAME de los contenedores ros-master y rosbridge

--- rosbridge/ros_entrypoint.sh
+++ rosbridge/ros_entrypoint.sh
@@ -4,4 +4,5 @@ set -e
 # setup ros environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source "/duckietown_ws/devel/setup.bash"
+export ROS_HOSTNAME="$(/bin/hostname -I)"
 exec "$@"
--- rosmaster/ros_entrypoint.sh
+++ rosmaster/ros_entrypoint.sh
@@ -4,4 +4,5 @@ set -e
 # setup ros environment
 source "/duckietown_ws/devel/setup.bash"
 source "/opt/ros/$ROS_DISTRO/setup.bash"
+export ROS_HOSTNAME="$(/bin/hostname -I)"
 exec "$@"
